511d28feb70f84312b81782e85ff1caa27bc58c2
Nitesh Singh <nitesh3692@gmail.com>
2024-01-19 15:53:13 +0530

    add files

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className='descriptionbox.navigator'>
+        <div className='descriptionbox-nav-box'>Description</div>
+        <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+        </div>
+        <div className='descriptionbox-description'>
+            <p>An e-commerce website is an online platform that facilitate the
+               buying and selling of products or services over the internet.
+              It servers as a virtual marketplace where businesses and individual
+              can showcase their products, interface with customers, and conductors
+               transactions without the need for a physical presence. E-commerce
+               websites have gained immense popularity due to their convenience
+               accessibility, and the global reach they offer.</p>
+               <p>
+                E-commerce website typically display products or services along 
+                detailed descriptions, images, prices, and any available variations
+                (e.g., size, colors). Eacg product usually has its own dedicated with relevant information. 
+               </p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+  const {getTotalCartItems}= useContext(ShopContext)
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" /> 
+        <p>MEZZO STORE</p>
+      </div>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop")}}
+        >
+          <Link  to="/">Shop</Link>
+          {menu === "shop" ? <hr/> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens")}}
+        >
+          <Link  to="/men">Men</Link>
+          {menu === "mens" ? <hr/> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens")}}
+        >
+          <Link to="/women">Women</Link>
+          {menu === "womens" ? <hr/>:<></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link  to="/kid">Kids</Link>
+          {menu === "kids" ? <hr/>:<></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
+        <button>Login</button></Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
