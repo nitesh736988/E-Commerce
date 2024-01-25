69fdacd10c1ba7564c49b23b220021916b0ee029
Nitesh Singh <nitesh3692@gmail.com>
2024-01-26 00:13:12 +0530

    add files

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './CSS/LoginSignup.css';
 
-
 const LoginSignup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      const response = await fetch('http://localhost:9696/api/authentication', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify({
+          FirstName:name,
+          EmailId:email,
+          Password:password,
+        }),
+      });
+console.log("response",response)
+      if (response.status===true) {
+        
+        console.log('Sign up successful');
+      } else {
+       
+        console.error('Sign up failed');
+      }
+    } catch (error) {
+      console.error('Error during sign up:', error.message);
+    }
+  };
+
+  
+
   return (
     <div className='loginsignup'>
       <div className='loginsignup-container'>
         <h1>Sign Up</h1>
         <div className='loginsignup-fields'>
-          <input type='text' placeholder='Your Name' />
-          <input type='email' placeholder='Email Address'/>
-          <input type='password' placeholder='Password' />
+          <input type='text' placeholder='Your Name' value={name} onChange={(e) => setName(e.target.value)} />
+          <input type='email' placeholder='Email Address' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         
-        <button>Continue</button>
+        <button onClick={handleSignUp}>Continue</button>
         <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
         <div className='loginsignup-agree'>
-          <input type = "checkbox" name='' id="" />
-          <p>By continue, i agree to the terms of use & privacy policy.</p>
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
         </div>
-       
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
